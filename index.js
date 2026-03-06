9fab6cd15cd97312f3ac915ad0790d059eb2f073
Felps-GS <felipeg.desouza4@gmail.com>
2026-03-06 10:08:21 -0300

    Calculo de idade

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 
+const dataAtual = new Date();
+
 
 app.get("/idade/:nome/:idade", (req, res) => {
     const { nome, idade } = req.params;
+    const idadeUsuario = dataAtual.getFullYear() - idade;
     res.status(200).json({
-      mensagem: `Nome: ${nome}, Ano de nascimento: ${idade} `
+      mensagem: `Nome: ${nome}, Idade: ${idadeUsuario} `
     })
 });
 
